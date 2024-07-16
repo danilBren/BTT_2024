7a6a147e19bef591b738df0e84a6fcf092f97b20
notGoodInNames <danilbren1@gmail.com>
2024-07-16 12:54:46 +0200

    _

---
+++ main.py
@@ -0,0 +1,37 @@
+def measComplete():
+    pass
+
+def measuring():
+    pass
+
+def pumping():
+    pass
+
+def waitingForSample():
+    pass
+
+def start():
+    pass
+
+def cleaning():
+    pass
+
+def unbinding():
+    pass
+
+states = [ start
+         , waitingForSample
+         , pumping
+         , measuring
+         , measComplete
+         , cleaning
+         , unbinding]
+
+curState = start
+prevState = start
+
+def run():
+    pass
+
+if __name__ == "__main__":
+    pass
