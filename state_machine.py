9c6ed21a87b7a3bad8f4b7f72a3d9ef128dac66f
notGoodInNames <danilbren1@gmail.com>
2024-08-27 15:08:53 +0200

    _

--- state_machine.py
+++ state_machine.py
@@ -29,7 +29,7 @@ def generate_random_number():
     """
     global call_count, previous_number
     
-    if call_count < 12:
+    if call_count < 10:
         # Calculate the step increment based on the number of calls
         step = (300 - 30) / 12
         min_val = previous_number
